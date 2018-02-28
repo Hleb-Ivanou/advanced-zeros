c4e7c45d5799cb0b4310b8e4210ddaa15c67fcf1
Hleb Ivanou <glebguru13@gmail.com>
2018-03-01 01:43:10 +0300

    implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var res = 0;
+  while (number) {
+    number = (number / getMaxPrime(base)) | 0;
+    res += number;
+  };
+
+  function getMaxPrime(x) {
+    var j = 0;
+    var i = 2;
+    var a = [];
+    var max = 0;
+    do {
+      if (x % i == 0) {
+        a[j] = i;
+        j++;
+        x = x / i;
+      } else {
+        i++;
+      }
+    }
+    while (i < x);
+    a[j] = i;
+    max = getMaxOfArray(a);
+    return max
+  }
+
+  function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+  }
+
+  return res;
 }
